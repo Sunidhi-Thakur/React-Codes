02c0c7e53b2f4538f46200c94bfe978329724bc0
Shubham <sunidhi21thakur@gmail.com>
2022-04-22 20:07:14 +0530

    Restaurant App

---
+++ restaurant-app/src/container/Chef/Chef.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import LoremIpsum from 'react-lorem-ipsum';
+
+
+import './Chef.css';
+
+const Chef = () => (
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img
+        src={images.chef}
+        alt='chef'
+      />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img
+            src={images.quote}
+            alt='quote'
+          />
+          <p className='p__opensans'><LoremIpsum avgSentencesPerParagraph={1} /></p>
+        </div>
+        <p className='p__opensans'><LoremIpsum avgSentencesPerParagraph={3}/></p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign"/>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Chef;
---
+++ restaurant-app/src/container/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
+import { LoremIpsum} from 'react-lorem-ipsum';
+
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chase the new flavour' />
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}><LoremIpsum /></p>
+      <button type='button' className='custom__button'>EXPLORE MENU</button>
+    </div>
+    <div className='app__wrapper_img'>
+    <img
+      src={images.welcome}
+      alt='header-img'
+    />
+    </div>
+  </div>
+);
+
+export default Header;
